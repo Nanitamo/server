dbffedbdd1b3368933c9c7b5342379930d1b3bdb
Nanitamo <nanitamo19@gmail.com>
2022-05-06 21:01:19 +0100

    back-end

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users',(req, res)=>{
+    
+    return res.send('Hello World');
+})
+app.listen(3333, ()=>{
+    console.log("HTTP SERVER RUNNING");
+});
+
+
+//SQLite 
+// Prisma
